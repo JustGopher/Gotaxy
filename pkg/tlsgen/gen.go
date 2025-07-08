35bfc8e86f28d13ab6327d68c882afec7f717e83
JustGopher <122082502+JustGopher@users.noreply.github.com>
2025-07-08 09:38:15 +0800

    feat: 添加签发tls证书工具函数 #3

---
+++ pkg/tlsgen/gen.go
@@ -0,0 +1,191 @@
+package tlsgen
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"log"
+	"math/big"
+	"net"
+	"os"
+	"time"
+)
+
+// helper 写文件
+func writePem(filename string, block *pem.Block) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			return
+		}
+	}(f)
+	return pem.Encode(f, block)
+}
+
+// GenerateCA 生成 CA 根证书及私钥，写入 dir/ca.crt 和 dir/ca.key
+func GenerateCA(dir string, validDays int) error {
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+
+	caKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return err
+	}
+
+	caTemplate := &x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			CommonName:   "GoRootCA",
+			Organization: []string{"MyOrg"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(0, 0, validDays),
+		IsCA:                  true,
+		BasicConstraintsValid: true,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+	}
+
+	caDER, err := x509.CreateCertificate(rand.Reader, caTemplate, caTemplate, &caKey.PublicKey, caKey)
+	if err != nil {
+		return err
+	}
+
+	err = writePem(dir+"/ca.key", &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(caKey)})
+	if err != nil {
+		return err
+	}
+
+	err = writePem(dir+"/ca.crt", &pem.Block{Type: "CERTIFICATE", Bytes: caDER})
+	if err != nil {
+		return err
+	}
+
+	log.Println("生成 CA 证书成功")
+	return nil
+}
+
+// GenerateServerAndClientCerts 基于已有 CA 生成 server 和 client 证书
+func GenerateServerAndClientCerts(dir string, validDays int, caCertPath, caKeyPath string) error {
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+
+	// 读取 CA 证书
+	caCertPEM, err := os.ReadFile(caCertPath)
+	if err != nil {
+		return err
+	}
+	block, _ := pem.Decode(caCertPEM)
+	if block == nil {
+		return errors.New("解析 CA 证书失败")
+	}
+	caCert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return err
+	}
+
+	// 读取 CA 私钥
+	caKeyPEM, err := os.ReadFile(caKeyPath)
+	if err != nil {
+		return err
+	}
+	block, _ = pem.Decode(caKeyPEM)
+	if block == nil {
+		return errors.New("解析 CA 私钥失败")
+	}
+	caKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return err
+	}
+
+	// =============== 生成服务端证书 =================
+	serverKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+	serialServer, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+	if err != nil {
+		return err
+	}
+	serverTemplate := &x509.Certificate{
+		SerialNumber: serialServer,
+		Subject: pkix.Name{
+			CommonName:   "localhost",
+			Organization: []string{"MyServer"},
+		},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().AddDate(0, 0, validDays),
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
+		IPAddresses: []net.IP{net.ParseIP("127.0.0.1")},
+		DNSNames:    []string{"localhost"},
+	}
+
+	serverDER, err := x509.CreateCertificate(rand.Reader, serverTemplate, caCert, &serverKey.PublicKey, caKey)
+	if err != nil {
+		return err
+	}
+
+	err = writePem(dir+"/server.key", &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(serverKey)})
+	if err != nil {
+		return err
+	}
+	err = writePem(dir+"/server.crt", &pem.Block{Type: "CERTIFICATE", Bytes: serverDER})
+	if err != nil {
+		return err
+	}
+
+	// =============== 生成客户端证书 =================
+	clientKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+	serialClient, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+	if err != nil {
+		return err
+	}
+	clientTemplate := &x509.Certificate{
+		SerialNumber: serialClient,
+		Subject: pkix.Name{
+			CommonName:   "client",
+			Organization: []string{"MyClient"},
+		},
+		NotBefore:   time.Now(),
+		NotAfter:    time.Now().AddDate(0, 0, validDays),
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
+	}
+
+	clientDER, err := x509.CreateCertificate(rand.Reader, clientTemplate, caCert, &clientKey.PublicKey, caKey)
+	if err != nil {
+		return err
+	}
+
+	err = writePem(dir+"/client.key", &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(clientKey)})
+	if err != nil {
+		return err
+	}
+	err = writePem(dir+"/client.crt", &pem.Block{Type: "CERTIFICATE", Bytes: clientDER})
+	if err != nil {
+		return err
+	}
+
+	log.Println("生成服务端和客户端证书成功")
+	return nil
+}
