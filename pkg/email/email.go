7483033a533309276929304985e217c898fb5c06
yq1822 <1927836028@qq.com>
2025-07-11 15:11:21 +0800

    feature(email): 添加邮件通知 (#20)
    feat: #14 添加邮件通知工具
    
    Co-authored-by: yf696 <14343199+yf696@user.noreply.gitee.com>

---
+++ pkg/email/email.go
@@ -0,0 +1,42 @@
+package email
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"regexp"
+
+	"gopkg.in/gomail.v2"
+)
+
+func SendEmail(to string, subject string, body string) error {
+	from := "1927836028@qq.com"
+	host := "smtp.qq.com"
+	port := 465
+	username := "1927836028@qq.com"
+	password := "rznfvafkkqqzjddj"
+
+	// 邮箱验证
+	emailPattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailPattern)
+	matchString := re.MatchString(to)
+	if !matchString {
+		err := errors.New("邮箱格式错误")
+		log.Println(err)
+		return err
+	}
+	m := gomail.NewMessage()
+	// 设置邮件消息的头部字段
+	m.SetHeader("From", from)       // 发送人
+	m.SetHeader("To", to)           // 接收人
+	m.SetHeader("Subject", subject) // 主题
+	m.SetBody("text/html", body)    // 正文内容
+	// 创建一个新的邮件拨号器对象，用于通过指定的 SMTP 服务器发送邮件
+	d := gomail.NewDialer(host, port, username, password)
+	// 通过拨号器对象发送指定的邮件消息
+	if err := d.DialAndSend(m); err != nil {
+		log.Printf("发送邮件失败: %v", err)
+		return fmt.Errorf("发送邮件失败: %w", err)
+	}
+	return nil
+}
