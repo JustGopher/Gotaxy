f30080b20bbdebbd6adc41b45bcafa3e741cf6e2
yf696 <14343199+yf696@user.noreply.gitee.com>
2025-07-17 13:38:23 +0800

    feature(log/service)：日志和服务
    - 重构日志
    - 添加开启和关闭服务的接口

--- internal/global/global.go
+++ internal/global/global.go
@@ -9,11 +9,12 @@ import (
 )
 
 var (
-	Ctx      context.Context
-	Cancel   context.CancelFunc
-	ConnPool *pool.Pool
-	InfoLog  *log.Logger
-	ErrorLog *log.Logger
-	DB       *sql.DB
-	Config   config.Config
+	Ctx       context.Context
+	Cancel    context.CancelFunc
+	ConnPool  *pool.Pool
+	InfoLog   *log.Logger
+	ErrorLog  *log.Logger
+	DB        *sql.DB
+	Config    config.Config
+	IsRunning int // 0:noRunning 1:running
 )
--- internal/web/service.go
+++ internal/web/service.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"github/JustGopher/Gotaxy/internal/global"
 	"github/JustGopher/Gotaxy/internal/tunnel/serverCore"
@@ -9,21 +10,62 @@ import (
 	"net/http"
 )
 
+// StatusService 服务状态
+func StatusService(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(map[string]interface{}{
+		"status": "success",
+		"data": map[string]int{
+			"isRunning": global.IsRunning,
+		},
+	})
+}
+
 // StartService 启动服务
 func StartService(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if global.IsRunning == 1 {
+		_ = json.NewEncoder(w).Encode(map[string]string{
+			"status":  "error",
+			"message": "服务已启动",
+		})
+		return
+	}
 	// 检查证书是否存在
 	if !tlsgen.CheckServerCertExist("certs") {
 		err := errors.New("证书缺失，请先生成证书")
-		_, _ = w.Write([]byte(err.Error()))
+		global.ErrorLog.Println("启动服务失败，err：", err)
+		_ = json.NewEncoder(w).Encode(map[string]string{
+			"status":  "error",
+			"message": "证书缺失，请先生成证书",
+		})
 		return
 	}
+	// 开启服务
 	global.Ctx, global.Cancel = context.WithCancel(context.Background())
 	go serverCore.StartServer(global.Ctx)
-	_, _ = w.Write([]byte("服务已启动"))
+	global.IsRunning = 1
+	_ = json.NewEncoder(w).Encode(map[string]interface{}{
+		"status": "success",
+		"data":   nil,
+	})
 }
 
 // StopService 停止服务
 func StopService(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if global.IsRunning == 0 {
+		_ = json.NewEncoder(w).Encode(map[string]string{
+			"status":  "error",
+			"message": "服务已停止",
+		})
+		return
+	}
+	// 停止服务
 	global.Cancel()
-	_, _ = w.Write([]byte("服务已停止"))
+	global.IsRunning = 0
+	_ = json.NewEncoder(w).Encode(map[string]interface{}{
+		"status": "success",
+		"data":   nil,
+	})
 }
